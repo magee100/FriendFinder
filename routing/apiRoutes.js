eab82aca1e3f0f049e10527da6ff20744cf36ca9
Andrew Magee <magee100@gmail.com>
2019-04-19 00:14:09 -0700

    set up html, working on posting new Friend to api/friends

---
+++ routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var friendData = require("../app/data/friends");
+
+module.exports = function(app){
+
+
+app.get("/api/friends", function(req, res){
+    res.json(friendData);
+});
+
+app.post("/api/friends", function(req, res){
+    friendData.push(req.body);
+    res.json(true)
+});
+
+};
